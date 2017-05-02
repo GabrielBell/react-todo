f2879fad7f4841ef2cab93573a9eead05d8f18f5
Gabriel Bell <gmbell23@gmail.com>
2017-05-02 16:26:25 -0400

    add auth reducer and actions

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -2,6 +2,22 @@ import moment from 'moment';
 
 import firebase, {firebaseRef, githubProvider} from 'app/firebase/';
 
+
+export var login = (uid) => {
+  return {
+    type: 'LOGIN',
+    uid
+  };
+};
+
+export var logout = () => {
+  return {
+    type: 'LOGOUT'
+  };
+};
+
+
+
 export var setSearchText = (searchText) => {
   return {
     type: 'SET_SEARCH_TEXT',
@@ -94,6 +110,7 @@ export var startLogin = () => {
   return (dispatch, getState) => {
     return firebase.auth().signInWithPopup(githubProvider).then((result) => {
       console.log('Auth worked!', result);
+      dispatch(login(result.user.uid));
     }, (error) => {
       console.log('Unable to auth', error);
     });
@@ -104,6 +121,7 @@ export var startLogout = () => {
   return (dispatch, getState) => {
     return firebase.auth().signOut().then(() => {
       console.log('Logged out!');
+      dispatch(logout());
     });
   };
 };
