e687b27fa0f59547865dbfca3f3590ef178caf05
GabrielBell <gmbell23@gmail.com>
2017-05-02 00:23:27 -0400

    Update index.js
    remove API key

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
 	var config = {
-	    apiKey: "AIzaSyAlcZf-Jb0entHOBdDJd8l1WuLtitbUgLA",
+	    
 	    authDomain: "bell-todo-app.firebaseapp.com",
 	    databaseURL: "https://bell-todo-app.firebaseio.com",
 	    projectId: "bell-todo-app",
 	    storageBucket: "bell-todo-app.appspot.com",
-	    messagingSenderId: "642371027946"
+	    
   	};
 
   	firebase.initializeApp(config);
