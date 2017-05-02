cd3f130e19c27002ce3f8cb8bece1771f9e59482
Gabriel Bell <gmbell23@gmail.com>
2017-05-02 13:58:39 -0400

    github login integration

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,10 +2,18 @@ import firebase from 'firebase';
 
 try {
 	var config = {
+<<<<<<< HEAD
 	    apiKey: process.env.API_KEY,
 	    authDomain: process.env.AUTH_DOMAIN,
 	    databaseURL: process.env.DATABASE_URL,
 	    storageBucket: process.env.STORAGE_BUCKET
+=======
+	    
+	    authDomain: "bell-todo-app.firebaseapp.com",
+	    databaseURL: "https://bell-todo-app.firebaseio.com",
+	    projectId: "bell-todo-app",
+	    storageBucket: "bell-todo-app.appspot.com",
+>>>>>>> e687b27fa0f59547865dbfca3f3590ef178caf05
 	    
   	};
 
